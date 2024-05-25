d61574cce9a56e187e09567a7d8ae651c2376321
MarOlivieri <141793518+MarOlivieri@users.noreply.github.com>
2024-05-25 11:13:04 +0000

    completo hasta el EX.11

--- app.js
+++ app.js
@@ -5,6 +5,29 @@ const fromEuroToDollar = function(valueInEuro) {
     // Retornamos el valor en dólares
     return valueInDollar;
 }// app.js file content
+
+
+// Declaramos una función con el nombre exacto "formEuroToDollar"
+//const fromDollarToYen = function(valueInDollar) {
+    // Convertimos el valor a Yen
+   // let valueInYen = valueInDollar * 156.5;
+    // Retornamos el valor en dólares
+    //return valueInDollar;
+
+// Declaramos una función con el nombre exacto "formYenToPound"
+
+const fromYenToPound = function(valueInYen) {
+    // Convertimos el valor a Pound
+    let valueInPound = valueInYen * 156.5;
+    // Retornamos el valor en Pound
+    return valueInYen;
+
+
+
+
+
+}// app.js file content
+
 console.log("Hello World")
 
 // This is my function that sums two numbers
@@ -17,4 +40,6 @@ console.log(sum(7,3))
 
 // Export the function to be used on other files 
 // (similar to the keyword "export" when using webpack)
-module.exports = { sum, fromEuroToDollar }
+module.exports = { sum, fromEuroToDollar,fromYenToPound}
+
+//comprobado también con "formEuroToDollar", si se prueba los tres al mismo tiempo da error
