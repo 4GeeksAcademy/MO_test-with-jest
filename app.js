24ba5e4116e009828ee61b69d7e43021580f1496
MarOlivieri <141793518+MarOlivieri@users.noreply.github.com>
2024-05-25 07:47:41 +0000

    pendiente terminar E.11

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-// app.js file content
+// Declaramos una función con el nombre exacto "formEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueInDollar;
+}// app.js file content
 console.log("Hello World")
 
 // This is my function that sums two numbers
@@ -11,4 +17,4 @@ console.log(sum(7,3))
 
 // Export the function to be used on other files 
 // (similar to the keyword "export" when using webpack)
-module.exports = { sum }
+module.exports = { sum, fromEuroToDollar }
