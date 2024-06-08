0689e15100aef974a63d988a8409465ea3d4a542
MarOlivieri <141793518+MarOlivieri@users.noreply.github.com>
2024-06-08 17:10:41 +0000

    verificado test

--- app.js
+++ app.js
@@ -23,4 +23,4 @@ const fromYenToPound = function(valueInYen) {
 }
 
 // Exportamos las funciones para que puedan ser utilizadas en los tests
-export default { fromEuroToDollar, fromDollarToYen, fromYenToPound };
+module.exports = { fromEuroToDollar, fromDollarToYen, fromYenToPound };
--- app.test.js
+++ app.test.js
@@ -1,23 +1,23 @@
 // Importamos las funciones desde app.js
-import { fromEuroToDollar, fromDollarToYen, fromYenToPound } from './app.js';
+const { fromEuroToDollar, fromDollarToYen, fromYenToPound } = require('./app.js');
 
 // Test para la conversión de euros a dólares
 test("One euro should be 1.07 dollars", function() {
     const dollars = fromEuroToDollar(3.5);
     const expected = 3.5 * 1.07;
-    expected (dollars).toBe(expected);
+    expect(dollars).toBe(expected);
 });
 
 // Test para la conversión de dólares a yenes
 test("One dollar should be 146.26 yen", function() {
     const yen = fromDollarToYen(1);
     const expected = (1 / 1.07) * 156.5;
-    expected(yen).toBeCloseTo(expected);
+    expect(yen).toBeCloseTo(expected);
 });
 
 // Test para la conversión de yenes a libras
 test("One yen should be 0.0056 pounds", function() {
     const pounds = fromYenToPound(1);
     const expected = (1 / 156.5) * 0.87;
-    expected(pounds).toBeCloseTo(expected);
+    expect(pounds).toBeCloseTo(expected);
 });
