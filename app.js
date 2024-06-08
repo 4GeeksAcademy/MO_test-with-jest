bf024db5e397d3e1c9656e53406a11c53214c1ed
MarOlivieri <141793518+MarOlivieri@users.noreply.github.com>
2024-06-08 16:38:58 +0000

    correcciones realizadas

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
-// Declaramos una función con el nombre exacto "formEuroToDollar"
+// Declaramos las tasas de conversión
+let oneEuroIs = {
+    "JPY": 156.5, // yen japonés
+    "USD": 1.07, // dólar estadounidense
+    "GBP": 0.87, // libra esterlina
+};
+
+// Función de conversión de euros a dólares
 const fromEuroToDollar = function(valueInEuro) {
-    // Convertimos el valor a dólares
-    let valueInDollar = valueInEuro * 1.07;
-    // Retornamos el valor en dólares
-    return valueInDollar;
+    return valueInEuro * oneEuroIs.USD;
 }
 
+// Función de conversión de dólares a yenes
+const fromDollarToYen = function(valueInDollar) {
+    let valueInEuro = valueInDollar / oneEuroIs.USD;
+    return valueInEuro * oneEuroIs.JPY;
+}
+
+// Función de conversión de yenes a libras
+const fromYenToPound = function(valueInYen) {
+    let valueInEuro = valueInYen / oneEuroIs.JPY;
+    return valueInEuro * oneEuroIs.GBP;
+}
+
+// Exportamos las funciones para que puedan ser utilizadas en los tests
+export default { fromEuroToDollar, fromDollarToYen, fromYenToPound };
